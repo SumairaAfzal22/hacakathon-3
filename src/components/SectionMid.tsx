94e49f55292480185b7e829f958e83dda7f3ae71
SumairaAfzal22 <sameerafzalkhan76@gmail.com>
2025-02-03 00:37:23 +0500

    Update SectionMid.tsx

--- src/components/SectionMid.tsx
+++ src/components/SectionMid.tsx
@@ -2,18 +2,16 @@ import React from 'react'
 import Image from "next/image"
 const SectionMid = () => {
   return (
-    <main>
-      <div className="relative w-full h-screen">
-
-        {/* Image Container */}
-       <div className='w-full h-auto max-w-[1349px] max-h-[700px] mx-auto'>
-              <Image className='w-[1349px] h-auto max-ss:w-[310px] max-ss:h-[235px]'
-              src={"/nike-2.png"}
-              alt="shoes"
-              width={1344}
-              height={700}/>
-              </div>
-      </div>
+    <main className='w-full h-full'>
+      <h1 className="text-[23px] font-medium mt-12">
+        Featured
+      </h1>
+      <div className='w-full'>
+        <Image className='w-full h-auto'
+          src={"/nike-2.png"}
+          alt="shoes"
+          width={1344}
+          height={700} /></div>
 
       <div className="text-center mt-[60px]">
         <h1 className="uppercase text-[30px] lg:text-[56px] font-medium text-black">STEP INTO WHAT FEELS GOOD</h1>
@@ -26,21 +24,22 @@ const SectionMid = () => {
           </button>
         </div>
       </div>
-     
-        <div className=' flex justify-between items-center text-[22px] font-semibold mb-4'>
-          <h1 className="text-black">Dont Miss</h1>
-        </div>
-        <div className="relative w-full h-screen">
-        <div className="absolute inset-0">
-          <img
-            src="/nike-3.png"
-            alt="Feature"
-            className="w-full h-full object-cover"
-          />
-        </div>
-        </div>
-      
+
+      <div className=' flex justify-between items-center text-[22px] font-semibold mb-4'>
+        <h1 className="text-black">Don&#39;t Miss</h1>
+      </div>
+
+      <div className='w-full'>
+        <Image className='w-full h-auto'
+          src={"/nike-3.png"}
+          alt="shoes"
+          width={1344}
+          height={700} />
+      </div>
+
     </main>
+
+
   )
 }
 
